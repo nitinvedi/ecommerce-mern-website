d7c7638542ed5ad5bf2fe086edf0f99f57e448a2
Nitin Chaturvedi <{chaturvediinitin@gmail.com}>
2025-11-10 23:02:09 +0530

    folder structure

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import app from './app.js';
+
+dotenv.config({path: '.env.local'});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('server is running');
+})

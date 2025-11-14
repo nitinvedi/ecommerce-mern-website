0f0ff8886102f0ca276024ef02a6525da1ceb30f
Nitin Chaturvedi <{chaturvediinitin@gmail.com}>
2025-11-14 20:03:10 +0530

    backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config({path: '.env.local'});
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Server response");
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,31 @@
-import dotenv from 'dotenv';
 import app from './app.js';
+import http from 'http';
+import {Server} from 'socket.io';
+import connectDB from './config/db.js';
 
-dotenv.config({path: '.env.local'});
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log('server is running on: http://localhost:' + PORT);
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    origin: '*',
+    methods: ['GET', 'POST']
+});
+io.on('connection', (socket) => {
+  console.log('New client connected', socket.id);
+
+  socket.on('trackRepair', (data) => {
+    console.log('Tracking data:', data);
+    socket.broadcast.emit('repairUpdate', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected', socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
